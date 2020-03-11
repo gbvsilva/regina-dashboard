aaf40347ecb39362208b9fe29f067d45e51f787f
gbvsilva <graco.babeuf18@gmail.com>
2020-03-11 16:14:24 -0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const headers={"fiware-service": "openiot", "fiware-servicepath": "/"};
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      sensorIDs: []
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+  
+  async handleClick () {
+    try {
+      const response = await axios({
+        "method": "get",
+        "url": "http://10.7.229.35:1026/v2/entities",
+        "headers": headers
+      });
+      console.log(response);
+      
+      var ids = [];
+      response.data.forEach(item => {
+        ids.push(item.id);
+      });
+
+      this.setState({sensorIDs: ids});
+
+      console.log('sensorIDs2 -> '+this.state.sensorIDs.toString());
+      return response;
+    }
+    catch(error) {
+      console.log(error);
+    }
+    
+  }
+
+  render () {
+    console.log('sensorIDs1 -> '+this.state.sensorIDs.toString());
+    return (
+      <div className='button__container'>
+        <button className='button' onClick={this.handleClick}>Click Me</button>
+        {this.state.sensorIDs.map(item => <p>{item}</p>)}
+      </div>
+    )
+  }
 }
 
 export default App;
